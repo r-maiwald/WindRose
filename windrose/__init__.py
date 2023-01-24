4bb1b0a86fc6af8f6a4f05f3db9c442a9a6f94a7
Robert Maiwald <rmaiwald@iup.uni-heidelberg.de>
2023-01-24 12:44:47 +0100

    Move to GitHub

--- windrose/__init__.py
+++ windrose/__init__.py
@@ -1,4 +1,1 @@
-from .windrose import Catalog, N_HEADER
-from . import utils
-
-__all__ = ["Catalog", "Simulation", "Status", "State", "N_HEADER"]
+from .windrose import Windrose
--- windrose/windrose.py
+++ windrose/windrose.py
@@ -12,9 +12,9 @@ def backward(x):
 
 
 class Windrose:
-    def __init__(self, r, theta, n_bins_r, n_bins_theta, fig):
+    def __init__(self, r, theta, n_bins_r, n_bins_theta, fig, subplot_id=None):
         # Init polar ax
-        ax = fig.add_subplot(projection="polar")
+        ax = fig.add_subplot(subplot_id, projection="polar")
         func_scale = FuncScale(ax, (forward, backward))
         ax.set_yscale(func_scale)
         ax.set_theta_offset(np.pi/2)
@@ -28,7 +28,7 @@ class Windrose:
         for i, theta_mid_point in enumerate(theta_mid_points):
             theta_min = theta_mid_point - theta_width / 2
             theta_max = theta_mid_point + theta_width / 2
-            index = (theta >= theta_min) & (theta < theta_max)
+            index = (theta % (2 * np.pi) >= theta_min) & (theta % (2 * np.pi) < theta_max)
             r_hist, r_bin_edges = np.histogram(
                 r[index], bins=n_bins_r, range=(r.min(), r.max())
             )
@@ -40,15 +40,18 @@ class Windrose:
         bottom = np.zeros(n_bins_theta)
         scaling_factor = 0.9
         for i in range(n_bins_r):
+            color = plt.cm.viridis(np.repeat(i / n_bins_r, n_bins_theta))
             ax.bar(
                 theta_mid_points,
                 values[:, i],
                 width=theta_width * scaling_factor,
                 bottom=bottom,
-                label="{:.1f} m/s to {:.1f} m/s".format(r_bin_edges[i], r_bin_edges[i+1]),
+                color=color,
+                label="{:.1f} to {:.1f}".format(r_bin_edges[i], r_bin_edges[i+1]),
             )
             bottom += values[:, i]
 
+        plt.legend(loc=(0.5, 0.2), title="Wind velocity in m/s")
         # Fixed formatter (not preferred)
         # ticks = ax.get_yticks()
         # ax.set_yticklabels(["{}%".format(int(i)) for i in ticks])
